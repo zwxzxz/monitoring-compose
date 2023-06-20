b9d1bf280fdb0dc0b3709f6086de203300e3ccf5
wenxin zheng <2574802744@qq.com>
2023-06-20 14:04:45 +0800

    优化脚本

--- stop.sh
+++ stop.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker compose -f docker-compose.yml -f extensions/mysqld-exporter-compose.yml -f extensions/node-exporter-compose.yml -f extensions/kafka-exporter-compose.yml -f extensions/redis-exporter-compose.yml down 
+docker compose -f docker-compose.yml \
+    -f extensions/mysqld-exporter-compose.yml \
+    -f extensions/node-exporter-compose.yml \
+    -f extensions/kafka-exporter-compose.yml \
+    -f extensions/redis-exporter-compose.yml 
+    down 
